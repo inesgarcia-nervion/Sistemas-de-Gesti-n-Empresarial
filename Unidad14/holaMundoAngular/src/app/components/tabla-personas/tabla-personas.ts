cfc18d9dc732e822401de0c5fcdc48e9963949eb
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-01-14 13:38:12 +0100

    Ejercicio 1 y 2

--- Unidad14/holaMundoAngular/src/app/components/tabla-personas/tabla-personas.ts
+++ Unidad14/holaMundoAngular/src/app/components/tabla-personas/tabla-personas.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Persona {
+  id: number;
+  nombre: string;
+  apellido: string;
+}
 
 @Component({
   selector: 'app-tabla-personas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabla-personas.html',
   styleUrl: './tabla-personas.css',
 })
 export class TablaPersonas {
-
+  personas: Persona[] = [
+      { id: 1, nombre: 'Inés', apellido: 'García' },
+      { id: 2, nombre: 'Ángela', apellido: 'García' },
+      { id: 3, nombre: 'Dylan', apellido: 'Cano' }
+    ];
 }
