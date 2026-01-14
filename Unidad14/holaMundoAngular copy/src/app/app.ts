b14b6003dce286805499a6f9ec9be084e0ef340d
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-01-14 14:19:19 +0100

    Ejercicio copiado

---
+++ Unidad14/holaMundoAngular copy/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TablaPersonas } from "./components/tabla-personas/tabla-personas";
+import { FormularioPersona } from "./components/formulario-persona/formulario-persona";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, TablaPersonas, FormularioPersona],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('holaMundoAngular');
+}
