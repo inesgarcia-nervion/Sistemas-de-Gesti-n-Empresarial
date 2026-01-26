c755d6eab95bf6eb72d7bf4f196160f42856ad4e
inesgarcia-nervion <ines.garcia@iesnervion.es>
2026-01-26 18:36:44 +0100

    Eliminación de ejercicios mal hechos

--- Unidad15/ListadoPersonas/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { PersonaListComponent } from './presentation/personas/components/persona-list/persona-list.component';
-
-export const routes: Routes = [
-	{ path: '', component: PersonaListComponent },
-];
--- Unidad15/ListadoPersonas/src/app/src/core/services/connection.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class ConnectionService {
-  // Return the base URL for the API. Adjust for your environment.
-  getConnectionString(): string {
-    return 'https://ines-frhqgndaghcnfpds.italynorth-01.azurewebsites.net';
-  }
-}
